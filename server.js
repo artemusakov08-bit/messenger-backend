69ba29b312e530a874876998d836864038853ad2
artemusakov08-bit <artemusakov08@gmail.com>
2025-11-19 22:51:18 +0300

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const io = new Server(server, {
   }
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 
 // Глобальный обработчик ошибок
 process.on('uncaughtException', (error) => {
