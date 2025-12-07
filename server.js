0535609581d808c21a510dbac9dba0d199199597
Artem Usakov <artemusakov08@gmail.com>
2025-12-07 17:53:05 +0300

    исправил все 16

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.use('/api/auth', authRoutes);
 const securityRoutes = require('./src/routes/security');
 app.use('/api/security', securityRoutes);
 app.use('/api/security', require('./src/routes/security'));
-app.use('/api/chat', chatRoutes); 
+app.use('/api/chats', chatRoutes);
 app.use('/api/call', callRoutes);
 app.use('/api/message', messageRoutes);
 
